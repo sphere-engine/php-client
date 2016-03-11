f28dca75fc9610e1434347441ba7b5b344b7f6b2
Robert Lewoń <rlewon@sphere-research.com>
2016-03-11 15:30:27 +0100

    Unit testy dla modułu Compilers

--- tests/ApiTest.php
+++ tests/ApiTest.php
@@ -28,4 +28,76 @@ class ApiTest extends PHPUnit_Framework_TestCase
         
         $this->assertEquals(3.14, $compilersClient->test()['pi']);
     }
+
+    public function testCompilersCompilersMethodSuccess()
+    {
+        $access_token = getenv("SE_ACCESS_TOKEN_COMPILERS");
+        $se = new SphereEngine\Api($access_token, "v3", "endpoint");
+        $compilersClient = $se->getCompilersClient();
+        
+        $this->assertEquals('C', $compilersClient->getCompilers()[11][0]);
+    }
+
+    public function testCompilersGetSubmissionMethodSuccess()
+    {
+        $access_token = getenv("SE_ACCESS_TOKEN_COMPILERS");
+        $se = new SphereEngine\Api($access_token, "v3", "endpoint");
+        $compilersClient = $se->getCompilersClient();
+        
+        $this->assertEquals(15, $compilersClient->getSubmission(38436074)['result']);
+    }
+
+    public function testCompilersGetSubmissionMethodNotExisting()
+    {
+        $access_token = getenv("SE_ACCESS_TOKEN_COMPILERS");
+        $se = new SphereEngine\Api($access_token, "v3", "endpoint");
+        $compilersClient = $se->getCompilersClient();
+
+        $this->assertEquals('ACCESS_DENIED', $compilersClient->getSubmission(9999999999)['error']);
+    }
+
+    public function testCompilersCreateSubmissionMethodSuccess()
+    {
+        $access_token = getenv("SE_ACCESS_TOKEN_COMPILERS");
+        $se = new SphereEngine\Api($access_token, "v3", "endpoint");
+        $compilersClient = $se->getCompilersClient();
+        
+        // TODO - lepiej na develu robić nowe zadania a jeszcze lepiej MOCK
+    }
+
+
+    public function testProblemsAutorizationFail()
+    {
+        $access_token = "fake access token";
+        $se = new SphereEngine\Api($access_token, "v3", "endpoint");
+        $problemsClient = $se->getProblemsClient();
+        try {
+            $problemsClient->test();
+            $this->assertTrue(false);
+        } catch (SphereEngine\SphereEngineResponseException $e) {
+            $this->assertTrue(true);
+        }
+    }
+
+    public function testProblemsAutorizationSuccess()
+    {
+        $access_token = getenv("SE_ACCESS_TOKEN_PROBLEMS");
+        $se = new SphereEngine\Api($access_token, "v3", "endpoint");
+        $problemsClient = $se->getProblemsClient();
+        try {
+            $problemsClient->test();
+            $this->assertTrue(true);
+        } catch (SphereEngine\SphereEngineResponseException $e) {
+            $this->assertTrue(false);
+        }
+    }
+
+    public function testProblemsTestMethodSuccess()
+    {
+        $access_token = getenv("SE_ACCESS_TOKEN_PROBLEMS");
+        $se = new SphereEngine\Api($access_token, "v3", "endpoint");
+        $problemsClient = $se->getProblemsClient();
+        
+        $this->assertEquals(true, count($problemsClient->test() > 0));
+    }
 }
