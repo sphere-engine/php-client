83a3e53ab0df5f4db0658593f8af4c40c8f93b25
Robert Lewoń <lewon.robert@gmail.com>
2016-04-21 11:57:47 +0200

    Spelling corrections

--- Examples/compilers/initialization/authError.php
+++ Examples/compilers/initialization/authError.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Example presents authorization error handling for 
+ * Example presents authorization error handling for
  * Sphere Engine Compilers API client
 */
 
@@ -13,10 +13,12 @@ require_once('../../../autoload.php');
 // define access parameters
 $accessToken = "wrong access token";
 $endpoint = getenv("SE_ENDPOINT_COMPILERS");
-$client = new CompilersClientV3($accessToken, $endpoint);
 
 // initialization
-try {	
+$client = new CompilersClientV3($accessToken, $endpoint);
+
+
+try {
 	$client->test();
 } catch (SphereEngineResponseException $e) {
 	if ($e->getCode() == 401) {
