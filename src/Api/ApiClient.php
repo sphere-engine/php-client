2cda2061e5f4496c03f2863208ca67f332d339a9
Robert Lewoń <rlewon@sphere-research.com>
2016-11-09 11:22:40 +0100

    Fix for systems with overwritten url & separaters

--- src/Api/ApiClient.php
+++ src/Api/ApiClient.php
@@ -91,7 +91,7 @@ class ApiClient
 	
 	    $curl = curl_init();
 	    // set timeout, if needed
-	    /* TODO: dobrze umiejscowi� ten timeout w kodzie
+	    /* TODO: make proper placement for timeout 
 	    if ($this->config->getCurlTimeout() != 0) {
 	        curl_setopt($curl, CURLOPT_TIMEOUT, $this->config->getCurlTimeout());
 	    }
@@ -103,7 +103,7 @@ class ApiClient
 	
 	    $queryParams['access_token'] = $this->accessToken;
 	    if (! empty($queryParams)) {
-	        $url = ($url . '?' . http_build_query($queryParams));
+	        $url = ($url . '?' . http_build_query($queryParams, '', '&'));
 	    }
 		
 	    if (is_array($postData)) {
