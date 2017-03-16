dcfe8a9fb175ffbed67278d8be9350653cb3720a
Robert Lewoń <lewon.robert@gmail.com>
2017-03-16 09:11:01 +0100

    Travis compatibility fix for PHP 7.x

--- tests/ProblemsClientV3ExceptionsNewTest.php
+++ tests/ProblemsClientV3ExceptionsNewTest.php
@@ -3,7 +3,12 @@
 use SphereEngine\Api\Mock\ProblemsClientV3;
 use SphereEngine\Api\SphereEngineResponseException;
 
-class ProblemsClientV3ExceptionsNewTest extends PHPUnit_Framework_TestCase
+// backward compatibility
+if (!class_exists('\PHPUnit\Framework\TestCase') && class_exists('\PHPUnit_Framework_TestCase')) {
+    class_alias('\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
+}
+
+class ProblemsClientV3ExceptionsNewTest extends \PHPUnit\Framework\TestCase
 {
 	protected static $client;
 	
